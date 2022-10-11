ac511ccc6187bb37e89d9b4bb8890f977aec1561
Gokhan Kurt <krtgkn@gmail.com>
2022-10-11 17:50:33 +0300

    build yoga win dlls without vcruntime dependency

--- build-win64.bat
+++ build-win64.bat
@@ -1,7 +1,6 @@
 rmdir /Q /s build
 mkdir build
 cd build
-@REM cmake -DCMAKE_CXX_COMPILER=cl.exe -A x64 ..
-cmake -G "MinGW Makefiles" ..
+cmake -DCMAKE_CXX_COMPILER=cl.exe -A x64 ..
 cmake --build . -- /property:Configuration=Release
 cd ..
